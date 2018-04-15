2247ba615759d1d32565ade0952c706a63df2664
Unknown <lucas546alexander@gmail.com>
2018-04-14 21:53:21 -0700

    Created file; Added doc header; Added section headers;
    Created racers.py; Added document header with file name, name of authors, date of file creation; Added section headers for import statements, variable declarations, function definitions, and the main program;

---
+++ racers.py
@@ -0,0 +1,15 @@
+########################################
+#                                      #
+# racers.py                            #
+# Authors: Lucas Eastman, Jamin Pottle #
+# Date: 4.14.18                        #
+#                                      #
+########################################
+
+# Import statements
+
+# Variable declarations
+
+# Function definitions
+
+# Main program
